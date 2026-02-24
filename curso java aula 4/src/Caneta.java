036db64e19176930d34761a05795af8ec21b5132
JapinhaMD <joaozinhory@hotmail.com>
2026-02-24 10:08:28 -0300

    Exercicio Caneta com Get e Set

--- curso java aula 4/src/Caneta.java
+++ curso java aula 4/src/Caneta.java
@@ -2,23 +2,23 @@ public class Caneta {
     public String modelo;
     private float ponta;
 
-    public Caneta(String m, float p){
-        this.modelo = m;
-        this.ponta = p;
-    }
 
     public String getModelo(){
         return this.modelo;
     }
+
     public void setModelo(String m){
         this.modelo = m;
     }
+
     public float getPonta(){
         return this.ponta;
     }
+
     public void setPonta(float p){
         this.ponta = p;
     }
+
     public void status(){
         System.out.println("SOBRE A CANETA");
         System.out.println("Modelo: " + this.getModelo());
